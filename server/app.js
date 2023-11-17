02b05a87e4955367bcb86e00ac2c3e86f9e496ce
Mikhail Dombrovsky <madombrovsky@gmail.com>
2023-11-17 22:39:35 +0200

    basic server

---
+++ server/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(cookieParser());
+
+app.listen()
